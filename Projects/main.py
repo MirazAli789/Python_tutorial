5fbaf67aba51b008aefd68875ca594d7eea0d215
Miraz <2609miraj@gmail.com>
2024-06-22 19:11:33 +0530

    he

--- Projects/main.py
+++ Projects/main.py
@@ -4,4 +4,7 @@ if __name__ == "__main__":
     print("Welcome to robospeaker")
     x = input("Enter word: ")
     command = f'powershell -Command "Add-Type -AssemblyName System.Speech; $synthesizer = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer; $synthesizer.Speak(\'Hello, {x}.\')"'
-    os.system(command)
+    try:
+        os.system(command)
+    except Exception as e:
+        print(e)
---
+++ Projects/u.py
@@ -0,0 +1,5 @@
+import os
+if __name__ == '__main__':
+    x = input("Enter a command \n")
+    command = x
+    os.system(command)
