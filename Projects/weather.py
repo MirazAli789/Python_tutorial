739ad4ed70397e0aa1765a54f3a72cfada24d21e
Miraz <2609miraj@gmail.com>
2024-06-25 20:45:34 +0530

    added modules

--- Projects/weather.py
+++ Projects/weather.py
@@ -1,6 +1,25 @@
+# using weather api ==> https://api.weatherapi.com/
+# 'requests' modules helps to get the http request (get request or post request)
+# 'json' module helps us to convert hte string into dictionary
 import requests
+import json
+import time
 
-city = input("Enter the name of hte city \n")
-url = f"https://api.weatherapi.com/v1/current.json?key=c86c83165cac49bdab1135835242206&q={city}"
-r = requests.get(url)
-print(r.text)
+while True:
+    print()
+    print("Welcome to Weather.com")
+    city = input("Enter the name of the city, or press q to close\n")
+    if city == "q":
+        break
+    url = f"https://api.weatherapi.com/v1/current.json?key=c86c83165cac49bdab1135835242206&q={city}"
+    r = requests.get(url)
+    dic = json.loads(r.text)
+    print("Searching ...")
+    time.sleep(1)
+    print("Current temparature:", dic["current"]["temp_c"], "°C")
+    print("Feels like:", dic["current"]["feelslike_c"], "°C")
+    print(dic["current"]["condition"]["text"])
+    print("Current wind speed:", dic["current"]["wind_mph"], "kmph")
+    print("Humidity:", dic["current"]["humidity"], "%")
+
+# print(r.text)
